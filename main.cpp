f2cfc3c9c24b1e7c8393d74664f7f53b25533e5f
Fernando Silveira <fsilveira@gmail.com>
2019-09-20 23:34:15 -0300

    Limit frame rate

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@
 int
 main(int argc, const char *argv[])
 {
+	const Uint32 MIN_FRAME_TIME = 16;
+
 	Game game;
 
-	game.init("Foobar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	game.init("foobar", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (game.running()) {
+		Uint32 frameTime = SDL_GetTicks();
+
 		game.handleEvents();
 		game.update();
 		game.render();
+
+		frameTime = SDL_GetTicks() - frameTime;
+		if (frameTime < MIN_FRAME_TIME)
+			SDL_Delay(MIN_FRAME_TIME - frameTime);
 	}
 
 	game.clean();
