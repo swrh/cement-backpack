b14623363762d08c9e1295df490dada8d702922d
Fernando Silveira <fsilveira@gmail.com>
2019-10-12 21:36:46 -0300

    Depend on SDL2

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 
 #include <iostream>
 
+#include <SDL.h>
+
 int
 main(int argc, char *argv[])
 {
-	std::cout << "Hello, world!\n" << std::flush;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "SDL_Init: " << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	SDL_Quit();
 
 	return EXIT_SUCCESS;
 }
